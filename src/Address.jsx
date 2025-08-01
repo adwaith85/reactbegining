b680307681f9b9a60dd1ac08647438bd0a0b8468
adwaith <adwaithadhu85227@gmail.com>
2025-08-01 15:36:00 +0530

    add1

--- src/Address.jsx
+++ src/Address.jsx
@@ -3,68 +3,86 @@ import React, { useEffect, useState } from "react";
 // import React, { useEffect } from "react";
 import PostCard from "./PostCard";
 import Card from 'react-bootstrap/Card';
+
 import Table from 'react-bootstrap/Table';
 
-function Address(){
-const [name,setName]=useState(0)
-const [item,setItem]=useState([])
-const onsubmit=async()=>{
+
+function Address() {
+
+  const [name, setName] = useState()
+  const [item, setItem] = useState([])
+  const onsubmit = async () => {
 
 
-let link=`https://api.postalpincode.in/pincode/${name}`
-let res= await fetch(link)
-let data = await res.json()
-setItem(data[0]["PostOffice"])
+    let link = `https://api.postalpincode.in/pincode/${name}`
+    let res = await fetch(link)
+    let data = await res.json()
+
+    setItem(data[0]["PostOffice"])
+    // alert(data[0]["PostOffice"][0].Name)
+  }
+
+  useEffect(() => {
+    const t = setTimeout(() => onsubmit(), 1000)
+
+    return () => clearTimeout(t)
+  }, [name])
+
+  return <div className="">
+
+    <center>
+      <h2 className="btn-calculator">pincode:{name ?? ""}</h2>
+    </center>
+
+    <input style={{ justifyItems: "", justifyContent: "start", marginLeft: 55 }} type="text" onChange={(e) => setName(e.target.value)} />
+    {/* <button  className="btn-submit" onClick={onsubmit}>submit</button> */}
+    {
+      item ? item.map(item => <PostCard name={item.Name} st={item.State} ds={item.DeliveryStatus} bt={item.BranchType} dec={item.Division} dis={item.District} reg={item.Region} con={item.Country} pin={item.Pincode} />) :
+        <><br />no data found</>
+    }
+  </div>
 }
 
-    return <div className="">
-        
-        <center>
-            <h2 className="btn-calculator">pincode:{name}</h2>
-        </center>
-          
-        <input style={{justifyItems:"flex-end", justifyContent:"start"}} type="text" onChange={(e)=>setName(e.target.value)} />
-        <button  className="btn-submit" onClick={onsubmit}>submit</button>
-        <Table striped bordered hover size="sm">
-      <thead>
-        <tr>
+
+
+
+
+
+{/* <Table striped bordered hover  border="2" style={{border:"3px solid black",
+       background:" linear-gradient(30deg,#0753,#3c3eaf)",
+       margin:"5px",
+      
+       fontWeight:"bold"}}>
+      <thead >
+        <tr style={{fontSize:"20px", fontWeight:"bolder",fontFamily:"Montserrat", }}>
           
           <th> Name</th>
           <th>Division</th>
           <th>District</th>
         </tr>
-      </thead>
-      {
-        item.map(item=><TableItem Name={item.Name} Division={item.Division} District={item.District}  />)
-      }
-      </Table>
+      </thead> */}
+{/* {
+        item?item.map(item=><PostCard Name={item.Name} Division={item.Division} District={item.District}  />):
+        <>no data found</>
+      } */}
+{/* </Table> */ }
 
 
 
 
-            
-      
-        
-        {/* <Card border="primary" style={{ width: 'auto', background: 'linear-gradient(30deg,#0753,#3c3eaf)', display: 'flex' }}>
-            <Card.Body style={{display:'flex',flexWrap:'wrap'}}>
-        {
-            item.map(item=><PostCard name={item.Name} dec={item.Division} dis={item.District} />)
-        }
-        </Card.Body>
-        </Card> */}
-        
-    </div>
-}
 
-function TableItem(props){
 
-    return<tr>
-            <td>{props.Name}</td>
-            <td>{props.Division}</td>
-            <td>{props.District}</td>
-            <td></td>
-        </tr>
-}
+
+
+// function TableItem(props){
+
+//     return<tr >
+//             <td>{props.Name}</td>
+//             <td>{props.Division}</td>
+//             <td>{props.District}</td>
+//             <td></td>
+//         </tr>
+// }
 
 
 
--- src/PostCard.jsx
+++ src/PostCard.jsx
@@ -1,19 +1,42 @@
-function PostCard(props){
+import Accordion from 'react-bootstrap/Accordion';
 
-    return<>
-    <div style={{border:"3px solid black",
-       background:" linear-gradient(30deg,#0753,#3c3eaf)",
-       margin:"5px",
-      
-       fontWeight:"bold"}}>
-      
-  <h1 style={{fontSize:"20px", fontWeight:"bolder",fontFamily:"Montserrat"}}>Name: {props.name}</h1>
-  <p>Division:{props.dec}<br/>
-    District:{props.dis}
-  </p>
+function PostCard(props) {
+
+  return <>
+    <div style={{
+      border: "3px solid black",
+      borderRadius: 10,
+      background: " linear-gradient(30deg,#0753,#3c3eaf)",
+      margin: "5px",
+
+      fontWeight: "bold"
+    }}>
+      <Accordion >
+        <Accordion.Item eventKey="0">
+          <Accordion.Header ><h1 style={{ fontSize: "20px", fontWeight: "bolder", fontFamily: "Montserrat" }}> {props.name}</h1>
+          </Accordion.Header>
+          <Accordion.Body>
+
+            <h5 style={{ fontSize: "25px", fontWeight: "bolder", fontFamily: "Montserrat", marginLeft: 160 }}>{props.pin}</h5>
+
+            <div style={{ fontWeight: "bold", fontFamily: "Montserrat", margin:5 }}> 
+              <h6 >BranchType</h6>
+                <h5><b>{props.bt}</b></h5><br />
+              <h6>location:</h6>
+              <h5><b>{props.dis}/{props.st}/{props.con}</b></h5><br />
+              <h6>Region:</h6>
+              <h5><b>{props.reg}</b></h5><br />
+              <h6>Division</h6>
+              <h5><b>{props.dec}</b></h5><br />
+              <h6>DeliveryStatus</h6>
+              <h5><b>{props.ds}</b></h5><br />
+            </div>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </div>
-  
-    </>
+
+  </>
 }
 
 export default PostCard
